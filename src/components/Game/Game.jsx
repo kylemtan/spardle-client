628e670bceb5ec4d90c7d50e90db31832f4f271a
kylemtan <kylemacasillitan@gmail.com>
2022-03-04 12:30:22 -0500

    hello

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -12990,7 +12990,7 @@ function Game(props) {
     "z",
   ];
 
-  const [seconds, setSeconds] = useState(2);
+  const [seconds, setSeconds] = useState(600);
 
   const [entries, setEntries] = useState([
     ["", "", "", "", ""],
